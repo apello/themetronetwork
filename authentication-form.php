530257f96c7899b000f9512970bdd996279301a9
apello <nur@Abdis-Air-8760.attlocal.net>
2021-04-04 18:00:44 -0400

    Left-over pages
    IDK what I did to these

--- authentication-form.php
+++ authentication-form.php
@@ -1,20 +1,22 @@
 <?php
 
 //require db and for future uses function page
-require_once("includes/db.php");
-require_once("includes/auth-functions.php");
 
 if(isset($_POST['submit'])){
+
+    require_once("includes/functions.php");
 
     $auth_username = $_POST["user-auth-name"];
     $auth_pwd = $_POST["user-auth-pwd"];
 
-    if(authIsEmpty($auth_username, $auth_pwd)) {
+    $filepath = "includes/db.php";
+
+    if(isEmpty($auth_username, $auth_pwd)) {
         header("Location: http://localhost:8888/themetronetwork/index.php?alert=missingvalue");
         exit();
     }
 
-    if(logIn($auth_username, $auth_pwd)) {        
+    if(logIn($auth_username, $auth_pwd, $filepath)) {        
         header("Location: http://localhost:8888/themetronetwork/main/feed.php");
         exit();
     } else {
--- main/search/account/account-view.php
+++ main/search/account/account-view.php
@@ -5,7 +5,7 @@
 	require_once("../../../includes/auth-check.php");
 	require_once("../../../includes/session-check.php");
 	//QUERIES FOR THIS PAGE
-	require_once("account-view-form.php");
+	require_once("account-view-select.php");
 
 
 	$_SESSION['LAST_ACTIVITY'] = time();
@@ -74,6 +74,28 @@
 
 </script> 
 
+<?php
+
+
+	$alert;
+
+
+	if($_GET['alert'] == 'error') {
+		$alert = 'Something went wrong! Please return to search <a href="../search.php">here</a> or use the back button.';
+	} else if ($_GET['alert'] == 'friended') {
+		$alert = 'You have successfully friended '. $user_account_info['first_name'].'!';
+	} else if ($_GET['alert'] == 'unfriended') {
+		$alert = 'You have successfully unfriended '. $user_account_info['first_name'].'!';
+	} else if ($_GET['id'] == '' OR $_GET['id'] == NULL) {
+		$alert = 'Something went wrong! Please return to search <a href="../search.php">here</a> or use the back button.';
+	} else if ($_GET == '' OR $_GET == NULL) {
+		$alert = 'Something went wrong! Please return to search <a href="../search.php">here</a> or use the back button.';
+	} else  {
+		$alert = 'User Account View';
+	}
+
+?>
+
 
 
 <body>
@@ -108,7 +130,7 @@
 
 					<div class="section section2" style="padding-bottom: 20px;">
 
-						<div class="section-title">User Account View</div>
+						<div class="section-title"><?php echo $alert ?></div>
 
 						<!-- USER INFO -->
 						<div class="content-box">
@@ -121,55 +143,10 @@
 							</div>
 						</div> 
 
-						<!-- FRIEND BTN -->
+						<!-- END USER INFO SECTION -->
 
-						<?php 
-						
-							//IF USER HAS FRIENDS
-							if($friends) {
 
-						?>
 
-						<form action="account-view.php" method="get">
-				
-
-							<button class="content-box friend">
-									<div class="full-content">
-										Unfriend <?php echo $user_account_info["first_name"];?>
-									</div>                   
-							</button>
-							
-							<input type="hidden" name="unfriend" value="<?php echo $_GET['id']; ?>">
-
-						</form>
-
-						<?php
-							//IF USER'S OWN ACCOUNT
-							} else if($own_account) {
-
-								echo'<div class="content-box" align="center">
-									<div class="full-content"><a href="../../settings/account/settings.php">Edit Account</a></div>
-								</div>';
-							//IF NONE OF THE ABOVE
-							} else {
-						?>
-							<form action="account-view.php" method="post">
-
-								<button class="content-box friend">
-										<div class="full-content">
-											Friend <?php echo $user_account_info["first_name"];?>
-										</div>                   
-								</button>
-								
-								<input type="hidden" name="friend" value="<?php echo $_GET['id']; ?>">
-
-							</form>
-						<?php
-							}
-
-						?>
-
-						
 
 						<!-- DUAL TITLE/DROPDOWN BAR - ONE FOR SHOW/ONE FOR HIDE -->
 						<div class="content-box" id="user-friend-show" style="margin-bottom: 5px;">
@@ -185,69 +162,77 @@
 						<!-- CONTENT USER SEES WHEN DROPDOWN -->
 						<div id="user-friend-content" style="margin-bottom: 30px;">
 
-						<?php
 
-							//PRACTIALLY THE SAME JOB AS $FRIENDS
+							<?php
 							if($has_friends) {
-								//ECHO AMOUNT OF FRIENDS 
+								//ECHO NUMBER OF COMMUNITIES JOINED
 								if($select_friends->rowCount() == 1) {
-									echo '<div class="content-box">
-											<div class="full-content">
-													They have 1 friend.
-											</div>
-										</div>';
-								} else if ($select_friends->rowCount() > 1) {
-									echo '<div class="content-box">
-											<div class="full-content">
-												They have '.$select_friends->rowCount().' friends.
-											</div>
-										</div>';
-								}
-
-								while($friends_acccount_info = $select_friends->fetch(PDO::FETCH_ASSOC)) {
-                                    //CONTENT BOX
-                                    echo ' 
-                                    <div class="content-box">
-                                        <div class="segment1">
-                                            <div class="result1">
-                                                <h2>';
-                    ?>
-
-													<!-- LINK TO VIEW PAGE -->
-                                                    <a href="../account/account-view.php?id=<?php echo $friends_acccount_info['id']; ?>">
-                                                        View
-                                                    </a>
-                    <?php  
-
-										//ACCOUNT INFO
-
-                                            echo '</h2>
-                                            </div>
-                                        </div>
-
-	
-
-                                        <div class="segment2">
-                                            <div class="result2">
-                                                <h2>'.$friends_acccount_info['first_name'] .' ' . $friends_acccount_info['last_name'] .'</h2>
-                                                <h3>'.$friends_acccount_info['username'].'</h3>
-                                            </div>
-                                        </div>
-                                    </div>';
-                                }
-
-							} else {
-
-								echo '<div class="content-box">
+							?>				
+									<div class="content-box">
 										<div class="full-content">
-											They have not friended anyone yet!
+											They have friended one user.
 										</div>
-									</div>';                   
-							}
+									</div>
 
-					?>
+							<?php
+								} else if ($select_friends->rowCount() > 1) {
+
+							?>
+									<div class="content-box">
+										<div class="full-content">
+											They have friended <?php echo $select_friends->rowCount(); ?> users.
+										</div>
+									</div>
+							<?php
+								} //IF END BRACKET
+
+								while($friend_acccount_info = $select_friends->fetch(PDO::FETCH_ASSOC)) {
+
+							?>
+															
+										<div class="content-box">
+											<div class="segment1">
+												<div class="result1">
+													<h2>
+														<!-- LINK TO VIEW PAGE -->
+														<a href="../account/account-view.php?id=<?php echo $friend_acccount_info['id']; ?>">
+															View
+														</a>
+													</h2>
+												</div>
+											</div>
+
+											<!-- ACCOUNT INFO -->
+
+											<div class="segment2">
+												<div class="result2">
+													<h2><?php echo $friend_acccount_info['first_name'] .' '. $friend_acccount_info['last_name'];?></h2>
+													<h3><?php echo $friend_acccount_info['username']; ?></h3>
+												</div>
+											</div>
+										</div>
+
+							<?php  	
+									} //WHILE LOOP END BRACKET
+
+								} else {
+							
+							?>
+
+								<div class="content-box">
+											<div class="full-content">
+												They have not friended anyone yet!
+											</div>
+										</div>
+										
+							<?php } ?>
 						
 						</div>
+
+
+						<!-- END USER FRIEND INFO SECTION -->
+
+
 
 						<!-- REPEAT OF FRIEND WITH COMMUNITIES INFO -->
 						<div class="content-box" id="comm-show">
@@ -263,71 +248,118 @@
 						<div id="communities-content" style="margin-bottom: 30px;">
 
 
-					<?php
-							if($communities) {
+							<?php
+								if($communities) {
+									//ECHO NUMBER OF COMMUNITIES JOINED
+									if($query->rowCount() == 1) {
+							?>				
+										<div class="content-box">
+											<div class="full-content">
+												They have joined one community.
+											</div>
+										</div>
+							
+							<?php
+									} else if ($query->rowCount() > 1) {
 
+							?>
+										<div class="content-box">
+											<div class="full-content">
+												They have joined <?php echo $query->rowCount(); ?> communities.
+											</div>
+										</div>
+							<?php
+									} //IF END BRACKET
 
-								//ECHO NUMBER OF COMMUNITIES JOINED
+									while($output = $query->fetch(PDO::FETCH_ASSOC)) {
+
+							?>
+										<!-- CONTENT BOX -->
 								
-								if($query->rowCount() == 1) {
-									echo '<div class="content-box">
-											<div class="full-content">
-												They have joined 1 community.
+										<div class="content-box">
+											<div class="segment1">
+												<div class="result1">
+													<h2>
+					
+														<a href="../communities/communities-view.php?id=<?php echo $output['id']; ?>">
+															View
+														</a>
+
+													</h2>
+												</div>
 											</div>
-										</div>';
-								} else if ($query->rowCount() > 1) {
-									echo '<div class="content-box">
-											<div class="full-content">
-												They have joined '.$query->rowCount().' communities.
+				
+											<div class="segment2">
+												<div class="result2">
+													<h2><?php echo $output['class_name']; ?></h2>
+													<h3><?php echo $output['class_proctor']; ?></h3>
+												</div>
 											</div>
-										</div>';
-								}
+										</div>
 
-								while($output = $query->fetch(PDO::FETCH_ASSOC)) {
+							<?php
+									} //WHILE LOOP END BRACKET
 
-                                    //CONTENT BOX
-                                    echo ' 
-                                    <div class="content-box">
-                                        <div class="segment1">
-                                            <div class="result1">
-                                                <h2>';
-                    ?>
-                                                    <a href="../communities/communities-view.php?id=<?php echo $output['id']; ?>">
-                                                        View
-                                                    </a>
-                    <?php  
-                                            echo '</h2>
-                                            </div>
-                                        </div>
-            
-                                        <div class="segment2">
-                                            <div class="result2">
-                                                <h2>'.$output['class_name'] .'</h2>
-                                                <h3>'.$output['class_proctor'].'</h3>
-                                            </div>
-                                        </div>
-                                    </div>';
-                                }
+								} else {
 
-							} else {
-
-								echo '<div class="content-box">
+							?>		
+									<div class="content-box">
 										<div class="full-content">
 											They have not joined any communities yet!
 										</div>
-									</div>';                   
-							}
+									</div>       
 
-					?>
+							<?php } //COMMUNITIES END BRACKET ?>
 
+						</div>
+
+						<!-- END COMMUNITIES INFO SECTION -->
 
 					</div>
 
-				</div>
+				</div>	
 
-			</div>	
+
+						<!-- FRIEND BTN -->
+
+						<!-- IF USER HAS FRIENDS AND IS NOT USERS OWN ACCOUNT -->
+						
+						<?php if($friends AND !$own_account) { ?>
+
+							<form action="account-view-form.php" method="post">
+
+
+								<button class="outer-button unfriend">
+									<img id="unfriend" src="../../../pictures/delete-friend.png">      
+								</button>
+								
+								<input type="hidden" name="unfriend" value="<?php echo $_GET['id']; ?>">
+								<input type="hidden" name="userid" value="<?php echo $row['id']; ?>">
+
+							</form>
+
+						<!-- IF NOT FRIENDS AND NOT USER'S OWN ACCOUNT -->
+
+						<?php } else if(!$friends AND !$own_account) { ?>
+
+							<form action="account-view-form.php" method="post">
+
+								<button class="outer-button friend">
+									<img id="friend" src="../../../pictures/plus.png">      
+								</button>
+														
+								<input type="hidden" name="friend" value="<?php echo $_GET['id']; ?>">
+								<input type="hidden" name="userid" value="<?php echo $row['id']; ?>">
+
+							</form>
+
+
+						<?php } //END IF BRACKET ?>
+
+						<!-- END FRIEND BTN -->
+
 		 
-		</div>
+			</div>
 
 		<?php include("../../../includes/footer.html"); ?>
 
